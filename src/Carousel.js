71540bb0c99f90dc08bf717809080a335f1bb999
Simon <simon.post.1999@gmail.com>
2021-11-30 02:14:16 -0500

    Fixed miscellaneous issues. Updated trucker image

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,6 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import image1 from './Content/Trucker-man.jpg';
+import image1 from './Content/trucker.jpg';
 import image2 from './Content/coronavirus-2.jpg';
 import image3 from './Content/CPU.jpg';
 
@@ -9,16 +9,16 @@ const DemoCarousel = () => {
         <Carousel>
             <div>
                 <img src={image1} alt=""/>
-                <p className="legend">Trucker</p>
+                <p className="legend">Trucker (placeholder)</p>
 
             </div>
             <div>
                 <img src={image2} alt=""/>
-                <p className="legend">Covid-19</p>
+                <p className="legend">Covid-19 (placeholder)</p>
             </div>
             <div>
                 <img src={image3} alt=""/>
-                <p className="legend">CPU</p>
+                <p className="legend">CPU (placeholder)</p>
             </div>
         </Carousel>
     );
