1612693e1c00aaf6ca14e7495d58587e350381c1
LoganQualk <loganq@gmail.com>
2021-12-04 13:43:49 -0500

    added some text to WhyYouShouldCare

--- src/WhyYouShouldCare.js
+++ src/WhyYouShouldCare.js
@@ -2,7 +2,11 @@ const WhyYouShouldCare = () => {
     return ( 
         <div className="container">
             <h1>Why You Should Care</h1>
-            <p className="centerText">Placeholder text</p>
+            <p className="centerText">
+                These supply chain issues do not just affect technology buffs, they affect everyone.  
+                From those of us that have smart homes to those of us that use flip phones, we all experience these hardships with the supply chain.  
+                
+            </p>
         </div>
      );
 }
