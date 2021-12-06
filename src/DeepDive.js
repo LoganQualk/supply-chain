5a320ece9c9bd136a82b5adf5e67ba071ca66937
LoganQualk <loganq@gmail.com>
2021-12-06 02:15:05 -0500

    added h3s

--- src/DeepDive.js
+++ src/DeepDive.js
@@ -7,7 +7,9 @@ const DeepDive = () => {
             <h3>What have the major effects been?</h3>
             <p>Placeholder text</p>
             <h3>How widespread are the supply chain issues?</h3>
-            <p>Placeholder text</p>
+            <p>
+                Believe it or not, supply chain issues have not only been affecting the United States, but they have been taking hold worldwide.
+            </p>
         </div>
      );
 }
--- src/WhyYouShouldCare.js
+++ src/WhyYouShouldCare.js
@@ -2,12 +2,14 @@ const WhyYouShouldCare = () => {
     return ( 
         <div className="container">
             <h1>Why You Should Care</h1>
+            <h3>Who does it affect?</h3>
             <p className="centerText">
                 These supply chain issues do not just affect technology buffs, they affect everyone.  
                 From those of us that have smart homes to those of us that use flip phones, we all experience these hardships with the supply chain.  
                 Issues with it have also affected other facets of society.  
                 There have been increases in delivery time for almost all products.  
             </p>
+            <h3>Why are these issues happening?</h3>
             <p className="centerText">
                 Issues with the supply chain due to covid will no doubt have long term affects on society as a whole.  
                 Due to a decline in freight drivers and the increase in freight prices, Catherine Macmillan talks about the issues the U.S. is 
