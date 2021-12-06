d9fffe0674b59d576c19608b425b119d99554290
LoganQualk <loganq@gmail.com>
2021-12-05 23:00:09 -0500

    added text to WhyYouShouldCare and tested spacing

--- src/WhyYouShouldCare.js
+++ src/WhyYouShouldCare.js
@@ -5,6 +5,12 @@ const WhyYouShouldCare = () => {
             <p className="centerText">
                 These supply chain issues do not just affect technology buffs, they affect everyone.  
                 From those of us that have smart homes to those of us that use flip phones, we all experience these hardships with the supply chain.  
+                Issues with it have also affected other facets of society.  
+                There have been increases in delivery time for almost all products.  
+            </p>
+            <br />
+            <p className="centerText">
+                Issues with the supply chain due to covid will no doubt have long term affects on society as a whole.  
                 
             </p>
         </div>
