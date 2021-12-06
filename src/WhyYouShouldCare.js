0b3e7153c39981d3ccd217d77fe0e436320ed3ee
LoganQualk <loganq@gmail.com>
2021-12-05 23:13:03 -0500

    2nd paragraph for WhyYouShouldCare

--- src/WhyYouShouldCare.js
+++ src/WhyYouShouldCare.js
@@ -11,7 +11,9 @@ const WhyYouShouldCare = () => {
             <br />
             <p className="centerText">
                 Issues with the supply chain due to covid will no doubt have long term affects on society as a whole.  
-                
+                Due to a decline in freight drivers and the increase in freight prices, Catherine Macmillan talks about the issues the U.S. is experiencing.  
+                <a href="https://www.smart-trucking.com/truck-driver-shortage/">Here</a> she states that we in the U.S. have experienced a number of missing drivers anywhere from 60,000 to 80,000.  
+                Obviously, this is a massive number and such numbers boils down to some simple reasons: low wages, poor working conditions, and the demands of the job.
             </p>
         </div>
      );
